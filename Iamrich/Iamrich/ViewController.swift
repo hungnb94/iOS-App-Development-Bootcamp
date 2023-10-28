ffbeff803d55d56c4f8cb560ec52ad645fe4f841
Hung Nguyen <hungnb94@gmail.com>
2023-10-28 17:14:29 +0700

    Init I am Rich project

---
+++ Iamrich/Iamrich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Hung Ba on 27/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
